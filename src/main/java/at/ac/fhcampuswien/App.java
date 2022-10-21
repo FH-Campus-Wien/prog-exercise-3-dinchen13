32087dd2a745ee803671c7816926b29d1f82b755
LAPTOP-4HEA5EIP\nadin <nadine.kirnbauer@stud.fh-campuswien.ac.at>
2022-10-21 09:37:43 +0200

    task 3 and 4 completed.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -9,13 +9,13 @@ public class App {
 
     public static void oneMonthCalendar(int numberOfTheDays, int firstDayOfMonth) {
 
-        for (int i = 1; i<firstDayOfMonth; i++){
+        for (int i = 1; i < firstDayOfMonth; i++) {
             System.out.print("   ");
         }
-        int sprung =firstDayOfMonth;
-        for (int i = 1; i<=numberOfTheDays;i++) {
+        int sprung = firstDayOfMonth;
+        for (int i = 1; i <= numberOfTheDays; i++) {
             System.out.printf("%2d ", i);
-            if ((sprung % 7 == 0)||(i==numberOfTheDays)) {
+            if ((sprung % 7 == 0) || (i == numberOfTheDays)) {
                 System.out.println();
             }
             sprung++;
@@ -42,22 +42,66 @@ public class App {
     }
 
 
-    public static long[] lcg(long seed){
+    public static long[] lcg(long seed) {
 
-        long [] randomNumbers= new long[10];
-        long m =(long) Math.pow(2,31);
+        long[] randomNumbers = new long[10];
+        long m = (long) Math.pow(2, 31);
         int c = 12345;
         int a = 1103515245;
-        randomNumbers[0]=(a*seed+c)%m;
-        for (int i = 1; i< randomNumbers.length; i++){
-            randomNumbers[i]=(a*randomNumbers[i-1]+c)%m;
+        randomNumbers[0] = (a * seed + c) % m;
+        for (int i = 1; i < randomNumbers.length; i++) {
+            randomNumbers[i] = (a * randomNumbers[i - 1] + c) % m;
         }
         return randomNumbers;
     }
 
 
+    public static void guessingGame(int numberToGuess) {
+
+        int guess = 0;
+        int counter = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.printf("Guess number %d: ", counter);
+            guess = scanner.nextInt();
+
+            if (guess == numberToGuess) {
+                System.out.println("You won wisenheimer!");
+            } else {
+                if ((guess > numberToGuess) && (counter != 10)) {
+                    System.out.println("The number AI picked is lower than your guess.");
+                } else if ((numberToGuess > guess) && (counter != 10)) {
+                    System.out.println("The number AI picked is higher than your guess.");
+                }
+                counter++;
+            }
+        } while (guess != numberToGuess && counter <= 10);
+
+        if (counter > 10) {
+            System.out.println("You lost! Have you ever heard of divide & conquer?");
+        }
+    }
+
+    public static int randomNumberBetweenOneAndHundred() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1;
+        return randomNumber;
+    }
 
 
+    public static boolean swapArrays(int[] array1, int[] array2) {
+        if (array1.length == array2.length) {
+            for (int i = 0; i < array1.length; i++) {
+                array1[i] = array1[i] + array2[i];
+                array2[i] = array1[i] - array2[i];
+                array1[i] = array1[i] - array2[i];
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -69,10 +113,27 @@ public class App {
         int firstDayOfMonth = 3;
         oneMonthCalendar(numberOfTheDays, firstDayOfMonth);
 
-        long seed=0;
+        long seed = 0;
         long[] randomNumbers = lcg(seed);
-        for (int i = 0; i<randomNumbers.length; i++) {
+        for (int i = 0; i < randomNumbers.length; i++) {
             System.out.println(randomNumbers[i]);
         }
+
+        int number = randomNumberBetweenOneAndHundred();
+        guessingGame(number);
+
+        int[] array1 = {1, 4, 6, 9};
+        int[] array2 = {7, 6, 3, 0};
+        swapArrays(array1, array2);
+        for (int j : array1) {
+            System.out.print(j);
+        }
+        System.out.println();
+        for (int j : array2) {
+            System.out.print(j);
+        }
+
+
+
     }
 }
